061d7cf3483b4f6a089a3a3ee49bcb7718d12445
Bodhish Thomas <bodhish@gmail.com>
2023-06-07 09:10:17 +0530

    Clean up unused code

--- app/dump_pupilfirst_schema.rb
+++
@@ -1,5 +0,0 @@
-require "graphql/client"
-require "graphql/client/http"
-require_relative "pupilfirst_api"
-
-GraphQL::Client.dump_schema(PupilfirstAPI::HTTP, "./schema.json")
--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -l
-
-ruby /entrypoint.rb
-
-output=$(cat /tmp/output.json)
-
-echo "::set-output name=result::$output"
