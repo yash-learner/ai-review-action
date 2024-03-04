dfcb06cfe2a9c8d8e0172b6a8ec498d846ef4477
yash-learner <yash.topaz@gmail.com>
2024-03-05 01:27:34 +0530

    Add function call for create_feedback

--- app/open_ai_client.rb
+++ app/open_ai_client.rb
@@ -1,111 +1,138 @@
-require 'openai'
-require 'yaml'
+require "openai"
+require "yaml"
 
 class OpenAIClient
   def initialize
     @client = OpenAI::Client.new
 
     @config = extract_relevant_step_configuration
-    @model = @config.fetch('OPEN_AI_MODEL', "gpt-3.5-turbo")
-    @temperature = @config.fetch('OPEN_AI_TEMPERATURE', 0.1).to_f
-    @system_prompt = @config.fetch('SYSTEM_PROMPT', system_prompt_default)
+    @model = @config.fetch("OPEN_AI_MODEL", "gpt-3.5-turbo")
+    @temperature = @config.fetch("OPEN_AI_TEMPERATURE", 0.1).to_f
+    @system_prompt = @config.fetch("SYSTEM_PROMPT", system_prompt_default)
   end
 
   def extract_relevant_step_configuration
     # Load workflow YAML file from the path specified in the environment variable or the default path.
-    file_path = ENV.fetch('WORKFLOW_FILE_PATH', './.github/workflows/ci.js.yml')
+    file_path = ENV.fetch("WORKFLOW_FILE_PATH", "./.github/workflows/ci.js.yml")
 
     # Find the job step that uses 'pupilfirst/ai-review-action' or has an ID containing 'ai-review'.
-    content = YAML.safe_load(File.read(file_path))
+    content = YAML.safe_load_file(file_path)
 
-    @config = content.dig('jobs', 'test', 'steps').find do |step|
-      ( step['uses']&.include?('pupilfirst/ai-review-action') || step['id']&.include?('ai-review') )
-    end['env']
+    @config = content.dig("jobs", "test", "steps").find do |step|
+      (step["uses"]&.include?("pupilfirst/ai-review-action") || step["id"]&.include?("ai-review"))
+    end["env"]
 
     if @config.nil?
       p content
 
-      raise 'Could not read configuration from environment variables. Please check the workflow file.'
+      raise "Could not read configuration from environment variables. Please check the workflow file."
     end
 
     @config
   end
 
+  def create_feedback_function
+    {
+      type: "function",
+      function: {
+        name: "create_feedback",
+        description: "Creates feedback for a student submission",
+        parameters: {
+          type: "object",
+          properties: {
+            feedback: {
+              type: "string",
+              description: "The feedback to be added to a student submission"
+            }
+          },
+          required: ["feedback"]
+        }
+      }
+    }
+  end
+
+  def function
+    create_feedback_function
+  end
+
   def ask
     puts prompt
     response = @client.chat(
-        parameters: {
-            model: @model,
-            messages: [
-                { role: "system", content: prompt }
-            ],
-            temperature: @temperature,
-        })
+      parameters: {
+        model: @model,
+        messages: [
+          {role: "system", content: prompt}
+        ],
+        tools: [function],
+        temperature: @temperature
+      }
+    )
     puts response
-    response.dig("choices", 0, "message", "content")
+    # response.dig("choices", 0, "message", "content")
+    response.dig("choices", 0, "message", "tool_calls", 0, "function", "arguments")
   end
 
   def prompt
     @system_prompt
-    .gsub("${ROLE_PROMPT}", default_role_prompt)
-    .gsub("${INPUT_DESCRIPTION}", default_input_prompt)
-    .gsub("${USER_PROMPT}", default_user_prompt)
-    .gsub("${SUBMISSION}", "#{Submission.new.checklist}")
-    .gsub("${OUTPUT_DESCRIPTION}", default_output_prompt)
+      .gsub("${ROLE_PROMPT}", default_role_prompt)
+      .gsub("${INPUT_DESCRIPTION}", default_input_prompt)
+      .gsub("${USER_PROMPT}", default_user_prompt)
+      .gsub("${SUBMISSION}", "#{Submission.new.checklist}")
+      .gsub("${OUTPUT_DESCRIPTION}", default_output_prompt)
   end
 
   def system_prompt_default
-<<-SYSTEM_PROMPT
-#{@config.fetch("ROLE_PROMPT", "${ROLE_PROMPT}")}
+    <<~SYSTEM_PROMPT
+      #{@config.fetch("ROLE_PROMPT", "${ROLE_PROMPT}")}
 
-#{@config.fetch("INPUT_DESCRIPTION", "${INPUT_DESCRIPTION}")}
+      #{@config.fetch("INPUT_DESCRIPTION", "${INPUT_DESCRIPTION}")}
 
-#{@config.fetch("USER_PROMPT", "${USER_PROMPT}")}
+      #{@config.fetch("USER_PROMPT", "${USER_PROMPT}")}
 
-#{@config.fetch("OUTPUT_DESCRIPTION", "${OUTPUT_DESCRIPTION}")}
-SYSTEM_PROMPT
+      #{@config.fetch("OUTPUT_DESCRIPTION", "${OUTPUT_DESCRIPTION}")}
+    SYSTEM_PROMPT
   end
 
   def default_role_prompt
-<<-ROLE_PROMPT
-You are an advanced Teaching Assistant AI. Your task involves reviewing and providing feedback on student submissions.
-ROLE_PROMPT
+    <<~ROLE_PROMPT
+      You are an advanced Teaching Assistant AI. Your task involves reviewing and providing feedback on student submissions.
+    ROLE_PROMPT
   end
 
   def default_user_prompt
-<<-USER_PROMPT
-The student's submission will be as follows:
-${SUBMISSION}
-USER_PROMPT
+    <<~USER_PROMPT
+      The student's submission will be as follows:
+      ${SUBMISSION}
+    USER_PROMPT
   end
 
   def default_input_prompt
-<<-INPUT_PROMPT
-The student's submissions will be an array of objects following the provided schema:
+    <<~INPUT_PROMPT
+      The student's submissions will be an array of objects following the provided schema:
 
-```json
-{
-  "kind": "The type of answer - can be shortText, longText, link, files, or multiChoice",
-  "title": "The question that was asked of the student",
-  "result": "The student's response",
-  "status": "Field for internal use; ignore this field during your review"
-}
-```
-INPUT_PROMPT
+      ```json
+      {
+        "kind": "The type of answer - can be shortText, longText, link, files, or multiChoice",
+        "title": "The question that was asked of the student",
+        "result": "The student's response",
+        "status": "Field for internal use; ignore this field during your review"
+      }
+      ```
+    INPUT_PROMPT
   end
 
   def default_output_prompt
-<<-OUTPUT_PROMPT
-Please provide your response in the following JSON format. Adhere to the format strictly and escape all line-breaks within strings using \\\\n.
+    <<~OUTPUT_PROMPT
+      Please provide your response in the following JSON format. Adhere to the format strictly and escape all line-breaks within strings using \\\\n.
 
-```json
-{
-    "status": "\"passed\" or \"failed\"",
-    "feedback": "Detailed feedback for the student in markdown format. Aim for a human-like explanation as much as possible."
-}
-```
+      ```json
+      {
+          "status": ""passed" or "failed"",
+          "feedback": "Detailed feedback for the student in markdown format. Aim for a human-like explanation as much as possible."
+      }
+      ```
 
-If the student submission is not related to question, share generic feedback.
-OUTPUT_PROMPT
+      If the student submission is not related to question, share generic feedback.
+    OUTPUT_PROMPT
   end
 end
--- entrypoint.rb
+++ entrypoint.rb
@@ -1,31 +1,31 @@
-require 'openai'
-require 'dotenv/load'
-require 'json'
-require 'pry'
-require_relative 'app/open_ai_client'
-require_relative 'app/submission'
-require_relative 'app/pupilfirst_api'
+require "openai"
+require "dotenv/load"
+require "json"
+require "pry"
+require_relative "app/open_ai_client"
+require_relative "app/submission"
+require_relative "app/pupilfirst_api"
 
 OpenAI.configure do |config|
-    config.access_token = ENV.fetch("OPENAI_ACCESS_TOKEN")
-    config.organization_id = ENV.fetch("OPENAI_ORGANIZATION_ID", "") # Optional.
+  config.access_token = ENV.fetch("OPENAI_ACCESS_TOKEN")
+  config.organization_id = ENV.fetch("OPENAI_ORGANIZATION_ID", "") # Optional.
 end
 
 def generate_response
-    @generate_response ||=
+  @generate_response ||=
     begin
-        JSON.parse(OpenAIClient.new.ask)
+      JSON.parse(OpenAIClient.new.ask)
     rescue => exception
-        {
-            status: "skip",
-            feedback: "Failure message"
-        }
+      {
+        status: "skip",
+        feedback: "Failure message"
+      }
     end
 end
 
 if ENV.fetch("SKIP_GRADING", "false") == "true"
-    PupilfirstAPI::Grader.new.add_feedback(generate_response)
+  PupilfirstAPI::Grader.new.add_feedback(generate_response)
 else
-    puts "Grading..."
-    PupilfirstAPI::Grader.new.grade(generate_response)
+  puts "Grading..."
+  PupilfirstAPI::Grader.new.grade(generate_response)
 end
